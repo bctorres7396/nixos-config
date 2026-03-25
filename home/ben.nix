30b3585391cb6426053d6d2f5312830f70e80d35
Ben <ben@nixlaptop.local>
2026-03-25 18:42:45 -0500

    add catppuccin kitty theme

--- home/ben.nix
+++ home/ben.nix
@@ -77,4 +77,31 @@
      ];
      
      fonts.fontconfig.enable = true;
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_size = "12.0";
+      background_opacity = "0.95";
+      background = "#1e1e2e";
+      foreground = "#cdd6f4";
+      cursor = "#f5e0dc";
+      selection_background = "#313244";
+      color0 = "#45475a";
+      color1 = "#f38ba8";
+      color2 = "#a6e3a1";
+      color3 = "#f9e2af";
+      color4 = "#89b4fa";
+      color5 = "#f5c2e7";
+      color6 = "#94e2d5";
+      color7 = "#bac2de";
+      color8 = "#585b70";
+      color9 = "#f38ba8";
+      color10 = "#a6e3a1";
+      color11 = "#f9e2af";
+      color12 = "#89b4fa";
+      color13 = "#f5c2e7";
+      color14 = "#94e2d5";
+      color15 = "#a6adc8";
+    };
+  };
   }
