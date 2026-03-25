74e0765a879fd2961fc6169f5b1d89e82e1ec79c
Ben <ben@nixlaptop.local>
2026-03-25 05:31:23 -0500

    fix hyprland startup

--- hosts/nixlaptop/configuration.nix
+++ hosts/nixlaptop/configuration.nix
@@ -92,7 +92,7 @@
       enable = true;
       settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
         user = "greeter";
       };
      };
