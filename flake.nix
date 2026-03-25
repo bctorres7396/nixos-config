3f3e76d96e9f8a7d0a6966a628a2360d9abbea91
Ben <ben@nixlaptop.local>
2026-03-24 19:03:26 -0500

    initial flake config with home-manager and hyprland

---
+++ flake.nix
@@ -0,0 +1,29 @@
+
+
+  {
+    description = "Ben's NixOS config";
+
+    inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+      home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ...}: {
+    nixosConfigurations.nixlaptop = nixpkgs.lib.nixosSystem {
+      system "x86_64-linux";
+      modules = [
+        ./hosts/nixlaptop/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ben = import ./home/ben.nix;
+        }
+      ];
+     };
+    };
+  }
