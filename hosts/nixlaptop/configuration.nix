be348178c86819d95a7620bf215012d2f02894f6
Ben <ben@nixlaptop.local>
2026-03-25 06:55:21 -0500

    add programs.hyprland.enable

--- hosts/nixlaptop/configuration.nix
+++ hosts/nixlaptop/configuration.nix
@@ -82,6 +82,7 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
+    programs.hyprland.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
