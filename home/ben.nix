5d1c968a2c5dca20045f89149cb80884c0fa37e4
Ben <ben@nixlaptop.local>
2026-03-26 11:10:20 -0500

    add hyprland animations and blur

--- home/ben.nix
+++ home/ben.nix
@@ -74,6 +74,29 @@
         ",XF86MonBrightnessUp, exec, brightnessctl set 10%+"
         ",XF86MonBrightnessDown, exec, brightnessctl set 10%-"
       ];
+      decoration = {
+        rounding = 10;
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+      };
+      drop_shadow = true;
+      shadow_range = 4;
+      shadow_render_power = 3;
+      };
+
+      animations = {
+        enabled = true;
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+  ];
+};
     };
   };
 
