4d55cf698a526e3edb40e115b1d0653c03072577
Ben <ben@nixlaptop.local>
2026-03-25 19:21:26 -0500

    add workspace keybindings

--- home/ben.nix
+++ home/ben.nix
@@ -19,6 +19,16 @@
             "SUPER, Q, killactive"
             "SUPER, M, exit"
             "SUPER, Space, exec, wofi --show drun"
+             "SUPER, 1, workspace, 1"
+             "SUPER, 2, workspace, 2"
+             "SUPER, 3, workspace, 3"
+             "SUPER, 4, workspace, 4"
+             "SUPER, 5, workspace, 5"
+             "SUPER SHIFT, 1, movetoworkspace, 1"
+             "SUPER SHIFT, 2, movetoworkspace, 2"
+             "SUPER SHIFT, 3, movetoworkspace, 3"
+             "SUPER SHIFT, 4, movetoworkspace, 4"
+             "SUPER SHIFT, 5, movetoworkspace, 5"
           ];
         };
       };
